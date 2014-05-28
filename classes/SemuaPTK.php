5e75dc0866e3009713b5933428c386869656ce5f
Dwi Irawan <dwiirawan08@gmail.com>
2014-05-28 21:09:42 +0700

    Create SemuaPTK.php

---
+++ classes/SemuaPTK.php
@@ -0,0 +1,36 @@
+<?php
+require_once ("class-config.php");
+
+class SemuaPTK
+{
+	public $results;
+	
+	public function __construct() {
+	}
+	
+	public function getResults(stdClass $params) {
+		try	{
+			$results = array();
+			
+			$db = Config::getInstance();
+			$query = "SELECT * FROM ptk order by nama";
+			$stmt = $db->prepare($query);
+			$stmt->execute();
+			$stmt->setFetchMode(PDO::FETCH_BOTH);
+			
+			while ($row = $stmt->fetch()) {
+				array_push($results, $row);
+			}
+			
+			return $results;
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+		$stmt->closeCursor();
+		$db=null;
+	}
+		
+	public function __destruct() {
+	}
+}
+?>
