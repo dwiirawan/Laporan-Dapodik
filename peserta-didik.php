117a78cda58fc6936a218ee127346fc975ae3ca1
Dwi Irawan <dwiirawan08@gmail.com>
2014-05-28 20:51:36 +0700

    Create peserta-didik.php

---
+++ peserta-didik.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Laporan Dapodik - Peserta Didik</title>
+	
+	<!--Ext JS-->
+	<link rel="stylesheet" type="text/css" href="extjs/resources/css/ext-all.css">
+	<script src="extjs/ext-all-debug.js"></script>
+	<!--Application JS-->
+	<script src="js/grid-pd.js"></script>
+	<script src="php/modules/pd/api-pd.php"></script>		
+	
+	<style type="text/css">
+		html, body {
+			height: 100%;
+		}
+		
+		html {
+			display: table;
+			margin: auto;
+		}
+		
+		body {
+			display: table-cell;
+		}
+
+	</style>
+	
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="fly">
+		<div class="content">
+			<?php include 'inc.menu.php'; ?>
+			<div class="ribbon">
+				<a href="https://www.facebook.com/groups/118554328323889/" target="_blank"><img src="images/facebook.png" height="30" alt="Group Operator Sekolah Kab. Kebumen"></a>
+			</div>
+		</div>
+	</div> 
+	
+	<div class="flying-robot"><img src="images/picture.png" alt=""></div>
+	
+	<div class="main">
+		<div class="content">
+		</div>
+	</div> 
+	
+	<div class="footer">
+		<?php include 'inc.footer.php' ?>
+	</div> 	
+</body>
+</html>
