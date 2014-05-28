d13166837e37bec08453f4fe85063cbc44bed28d
Dwi Irawan <dwiirawan08@gmail.com>
2014-05-28 20:48:32 +0700

    Create inc.footer.php

---
+++ inc.footer.php
@@ -0,0 +1,5 @@
+		<div class="content">
+			<p>
+				Copyright &copy; 2014 <a href="https://www.facebook.com/groups/118554328323889/" target="_blank">Operator Sekolah Kab. Kebumen, Jawa Tengah</a>
+			</p>
+		</div>
