47ad2606a77700b082ef30de75b81b9f25d89b90
Dwi Irawan <dwiirawan08@gmail.com>
2014-05-28 20:47:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Laporan Dapodik</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<div class="fly">
+		<div class="content">
+			<?php include 'inc.menu.php'; ?>
+			
+			<div class="ribbon">
+				<a href="https://www.facebook.com/groups/118554328323889/" target="_blank"><img src="images/facebook.png" height="30" alt="Group Operator Sekolah Kab. Kebumen"></a>
+			</div>
+		</div>
+	</div> <!-- /.fly -->
+	<div class="flying-robot"><img src="images/picture.png" alt=""></div>
+	<div class="main">
+		<div class="content">
+			<h2>DEMO | Lorem ipsum dolor sit amet</h2><br/>
+			<p><strong>Lorem</strong> ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi.</p>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio dolorum dicta similique suscipit veniam voluptates magnam id quisquam molestiae enim a quidem officiis nulla alias quos expedita harum laudantium ducimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ea quae soluta architecto provident quod molestiae voluptas nemo minima. Delectus assumenda accusamus dolorem inventore odit consequatur unde sit veniam laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi.</p>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi.</p>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi.</p>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel impedit officia necessitatibus animi minima dolores magni sit dignissimos dolore deserunt veniam vero eaque totam fugiat reprehenderit at blanditiis culpa similique. Tenetur aspernatur illum incidunt corporis eius quo ratione asperiores reiciendis ipsam odio deleniti cumque adipisci delectus corrupti nobis hic eos repellat repellendus quis rerum officiis dolor provident ea placeat accusantium iure illo doloremque repudiandae optio. Beatae blanditiis libero voluptas dignissimos neque hic alias culpa eius est expedita ducimus ad explicabo fugiat quam similique in labore eligendi asperiores nam magni. Amet consequatur debitis autem fugit labore omnis reiciendis quam veniam modi.</p>
+			
+		</div>
+	</div> <!-- /.main -->
+	
+	<div class="footer">
+		<?php include 'inc.footer.php' ?>
+	</div> <!-- /.footer -->
+		
+</body>
+</html>
